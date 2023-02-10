60f18afb6ba926052d74b14430c50ffd2d9c833e
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-02-10 07:14:41 -0500

    Add basic inquirer function.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-// TODO: Include packages needed for this application
+const inq=require('inquirer');
+const fs=require('fs');
+const md=require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {type:'input', message: 'What is the name of the project?', name:'sProjTitle'},
+    {type:'input', message: `Please enter a description of the project:`, name:'sProjDesc'},
+    {type:'input', message: 'Please enter installation instructions:', name:'sProjInstall'},
+    {type:'input', message: 'Please provide instructions on how to use this code:', name:'sProjUsage'},
+    {type:'input', message: 'Please describe contributions to this project', name:'sProjContribs'},
+    {type:'input', message: 'Please provide testing information:', name:'sProjTests'}
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(data);
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inq.prompt(questions).then((response) => writeToFile('README.md',response));
+
+}
 
 // Function call to initialize app
 init();
